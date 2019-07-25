4c5f950451c58d1f5a27fc47083a9f51b6a2fda0
Santiago Sanchez <chagostwo@yahoo.com>
2019-07-25 01:38:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
     end.first
   end
 
-  def slef.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
